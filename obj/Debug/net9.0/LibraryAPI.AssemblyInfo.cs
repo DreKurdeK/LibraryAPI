8075a7dbdbc3206d76b6a9865cd7860ea0a4971f
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 15:43:13 +0100

    Auto generated

--- obj/Debug/net9.0/LibraryAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ed834d2ce9e7d99d5cc262ed5e658c73f635b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d3ec55e65c6a0a79d146f67edd0b5301d3168b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
