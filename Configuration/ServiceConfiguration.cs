73c620bb6033e79635ab933cf55957271a2ba3f3
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 13:56:42 +0100

    Added Repositories and DbContext

--- Configuration/ServiceConfiguration.cs
+++ Configuration/ServiceConfiguration.cs
@@ -1,7 +1,10 @@
 ﻿using System.Diagnostics.Eventing.Reader;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using LibraryAPI.Data;
+using LibraryAPI.Repositories;
 using LibraryAPI.Validators;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryAPI.Configuration;
 
@@ -9,6 +12,15 @@ public static class ServiceConfiguration
 {
     public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
+        var conntectionString = configuration["ConnectionStrings:DefaultConnection"];
+        
+        services.AddDbContext<LibraryDbContext>(options =>
+            options.UseSqlServer(conntectionString));
+        
         services.AddValidatorsFromAssemblyContaining<BookValidator>();
+        
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<IAuthorRepository, AuthorRepository>();
+        services.AddScoped<IPublisherRepository, PublisherRepository>();
     }
 }
