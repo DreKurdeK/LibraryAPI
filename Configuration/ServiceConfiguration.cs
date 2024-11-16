d686252a5b6cf347f0762fd36305664b62227baf
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 11:33:08 +0100

    Added validation service

--- Configuration/ServiceConfiguration.cs
+++ Configuration/ServiceConfiguration.cs
@@ -1,9 +1,14 @@
-﻿namespace LibraryAPI.Configuration;
+﻿using System.Diagnostics.Eventing.Reader;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using LibraryAPI.Validators;
+
+namespace LibraryAPI.Configuration;
 
 public static class ServiceConfiguration
 {
     public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        
+        services.AddValidatorsFromAssemblyContaining<BookValidator>();
     }
 }
