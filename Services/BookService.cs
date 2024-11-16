b03edc913d7716c599d525e79d1767b659b3e9fc
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 15:14:17 +0100

    Added update and add method

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -1,12 +1,17 @@
-﻿using LibraryAPI.Exceptions;
+﻿using AutoMapper;
+using LibraryAPI.DTOs;
+using LibraryAPI.Exceptions;
 using LibraryAPI.Models;
 using LibraryAPI.Repositories;
 
 namespace LibraryAPI.Services;
 
-public class BookService(IBookRepository bookRepository) : IBookService
+public class BookService(IBookRepository bookRepository, IAuthorRepository authorRepository, IPublisherRepository publisherRepository, IMapper mapper) : IBookService
 {
     private readonly IBookRepository _bookRepository = bookRepository;
+    private readonly IAuthorRepository _authorRepository = authorRepository;
+    private readonly IPublisherRepository _publisherRepository = publisherRepository;
+    private readonly IMapper _mapper = mapper;
 
     public async Task<List<Book>> GetAllBooksAsync()
     {
@@ -24,20 +29,39 @@ public class BookService(IBookRepository bookRepository) : IBookService
         return book;
     }
 
-    public async Task AddAsync(Book book)
+    public async Task AddAsync(BookDTO bookDto)
     {
+        var author = await _authorRepository.GetByIdAsync(bookDto.AuthorId);
+        if (author == null) throw new AuthorNotFoundException(bookDto.AuthorId);
         
+        var publisher = await _publisherRepository.GetByIdAsync(bookDto.PublisherId);
+        if (publisher == null) throw new PublisherNotFoundException(bookDto.PublisherId);
+        
+        var book = _mapper.Map<Book>(bookDto);
+        book.Author = author;
+        book.Publisher = publisher;
+        
+        await _bookRepository.AddAsync(book);
     }
 
-    public async Task UpdateAsync(Book book)
+    public async Task UpdateAsync(Guid id, BookDTO bookDto)
     {
-        var existingBook = await _bookRepository.GetByIdAsync(book.Id);
+        var existingBook = await _bookRepository.GetByIdAsync(id);
         if (existingBook == null)
         {
-            throw new BookNotFoundException(book.Id);
+            throw new BookNotFoundException(id);
         }
+        _mapper.Map(bookDto, existingBook);
         
-        await _bookRepository.UpdateAsync(book);
+        var author = await _authorRepository.GetByIdAsync(bookDto.AuthorId);
+        if (author == null) throw new AuthorNotFoundException(bookDto.AuthorId);
+        existingBook.Author = author;
+        
+        var publisher = await _publisherRepository.GetByIdAsync(bookDto.PublisherId);
+        if (publisher == null) throw new PublisherNotFoundException(bookDto.PublisherId);
+        existingBook.Publisher = publisher;
+        
+        await _bookRepository.UpdateAsync(existingBook);
     }
 
     public async Task DeleteAsync(Guid id)
