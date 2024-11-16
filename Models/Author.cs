f88ad2af9d7a30118bff41304f9a26731db5d9e1
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 11:36:03 +0100

    Removed validation using attributes

--- Models/Author.cs
+++ Models/Author.cs
@@ -5,16 +5,7 @@ namespace LibraryAPI.Models;
 public class Author
 {
     public Guid Id { get; set; }
-    
-    [Required(ErrorMessage = "First name is required")]
-    [StringLength(100, ErrorMessage = "First name cannot be longer than 100 characters")]
     public string FirstName { get; set; }
-    
-    [Required(ErrorMessage = "Last name is required")]
-    [StringLength(100, ErrorMessage = "Last name cannot be longer than 100 characters")]
     public string LastName { get; set; }
-    
-    [Required(ErrorMessage = "Date of Birth is required")]
-    [DataType(DataType.Date, ErrorMessage = "Date of Birth must be a date")]
     public DateTime DateOfBirth { get; set; }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -7,19 +7,10 @@ public class Book
     public Guid Id { get; set; } // Primary Key
     public string Title { get; set; }
     public string ISBN { get; set; }
-    
-    [Required(ErrorMessage = "Category is required")]
     public BookCategory Category { get; set; }
-    
-    [Required(ErrorMessage = "Author id is required")]
     public Guid AuthorId { get; set; } 
     public Author Author { get; set; } 
-    
-    [Required(ErrorMessage = "Publisher id is required")]
     public Guid PublisherId { get; set; }
     public Publisher Publisher { get; set; }
-    
-    [Required(ErrorMessage = "Date is required")]
-    [DataType(DataType.Date, ErrorMessage = "Date must be a valid date")]
     public DateTime ReleaseDate { get; set; }
 }
