39325e795d8b9fe7ed191bb70039b8ea90cd99b9
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 11:33:43 +0100

    Added Publisher Validator

---
+++ Validators/PublisherValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Validators;
+
+public class PublisherValidator : AbstractValidator<Publisher>
+{
+    public PublisherValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("Publisher name cannot be empty.")
+            .Length(1, 200).WithMessage("Publisher name must be between 1 and 200 characters.");
+
+        RuleFor(p => p.Address)
+            .MaximumLength(500).WithMessage("Adress cannot be longer than 500 characters.");
+
+        RuleFor(p => p.FoundedYear)
+            .InclusiveBetween(1800, DateTime.Now.Year).WithMessage($"Date of year must be between 1800 and {DateTime.Now.Year}.");
+    }
+}
