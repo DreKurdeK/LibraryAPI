bf8b09363c9d33c6e041c267c6ba8029eff3da62
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 15:28:28 +0100

    Added Auto Mapper Profile

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using LibraryAPI.DTOs;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Helpers;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<AuthorDTO, Author>();
+        CreateMap<Author, AuthorDTO>();
+        
+        CreateMap<BookDTO, Book>();
+        CreateMap<Book, BookDTO>();
+        
+        CreateMap<PublisherDTO, Publisher>();
+        CreateMap<Publisher, PublisherDTO>();
+    }
+}
