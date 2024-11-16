f49aa493e4d6e1cf335715233053315c98857dfc
DreKurdeK <radziak.daniel@o2.pl>
2024-11-16 14:13:52 +0100

    Added DbContext

---
+++ Data/LibraryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Data;
+
+public class LibraryDbContext : DbContext
+{
+    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+    
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Publisher> Publishers { get; set; }
+}
