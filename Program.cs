d2a438ae998a5d8eda71436c1bdc9d8d31ef7e2d
DreKurdeK <radziak.daniel@o2.pl>
2024-11-17 19:54:41 +0100

    Added routing

--- Program.cs
+++ Program.cs
@@ -2,16 +2,20 @@ using static LibraryAPI.Configuration.ServiceConfiguration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureServices(builder.Services, builder.Configuration);
+await ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
+app.UseExceptionHandler("/error");
+
 if (app.Environment.IsDevelopment())
 {
-    
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 app.MapControllers();
 app.Run();
 
