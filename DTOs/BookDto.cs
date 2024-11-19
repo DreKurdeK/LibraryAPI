07bfd97cc0b3400d3a6a42c1c2145471fba16d06
DreKurdeK <radziak.daniel@o2.pl>
2024-11-19 12:08:32 +0100

    Added Id to model

--- DTOs/BookDto.cs
+++ DTOs/BookDto.cs
@@ -4,6 +4,7 @@ namespace LibraryAPI.DTOs;
 
 public class BookDto
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public string ISBN { get; set; }
     public BookCategory Category { get; set; }
