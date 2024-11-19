6277c54be9e1766915684a266b298f046030cdcd
DreKurdeK <radziak.daniel@o2.pl>
2024-11-19 12:17:09 +0100

    Added Guid field

--- DTOs/AuthorDto.cs
+++ DTOs/AuthorDto.cs
@@ -2,6 +2,7 @@
 
 public class AuthorDto
 {
+    public Guid Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime DateOfBirth { get; set; }
--- DTOs/PublisherDto.cs
+++ DTOs/PublisherDto.cs
@@ -2,6 +2,7 @@
 
 public class PublisherDto
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
     public int FoundedYear { get; set; }
