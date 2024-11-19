bec3d7c3699b53f2178258faccec8790b5ec3d79
DreKurdeK <radziak.daniel@o2.pl>
2024-11-19 15:06:48 +0100

    Added PagedResult model

---
+++ Models/PagedResult.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryAPI.Models;
+
+public class PagedResult<T>
+{
+    public int TotalItems { get; set; } // Total items
+    public IEnumerable<T> Items { get; set; } // Items on page
+}
