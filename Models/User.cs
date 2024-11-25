de93f965e6fc8dac61f1d5a3480f2d29191d349f
DreKurdeK <radziak.daniel@o2.pl>
2024-11-25 16:07:22 +0100

    Added required modifier

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,11 @@
-﻿namespace LibraryAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+namespace LibraryAPI.Models;
 
 public class User
 {
     public Guid Id { get; set; }
-    public string Username { get; set; }
+    public required string Username { get; set; }
     public string PasswordHash { get; set; }
-    public string Email { get; set; }
-    public ICollection<UserRole> UserRoles { get; set; }
+    public required string Email { get; set; }
+    public required ICollection<UserRole> UserRoles { get; set; }
 }
