50642dd2d35a2f10899e62b4c47681fb0ec70018
DreKurdeK <radziak.daniel@o2.pl>
2024-11-25 13:03:02 +0100

    Added Change password method to controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,8 @@
-﻿using LibraryAPI.Models;
+﻿using System.Security.Claims;
+using LibraryAPI.Models;
 using LibraryAPI.Services;
+using LibraryAPI.Validators;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryAPI.Controllers;
@@ -22,5 +25,23 @@ public class AuthController(IUserService userService) : ControllerBase
     {
         var token = await _userService.LoginAsync(loginRequest);
         return Ok(new { Token = token });
+    }
+    
+    [Authorize]
+    [HttpPost("change-password")]
+    public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequest request)
+    {
+        var validator = new ChangePasswordRequestValidator();
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.Errors);
+        }
+
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new InvalidOperationException("Something went wrong."));
+        var result = await _userService.ChangePasswordAsync(userId, request);
+
+        return Ok(result);
     }
 }
