23868f793ac84e7619974fad0aae5acccce062ee
DreKurdeK <radziak.daniel@o2.pl>
2024-11-25 12:58:05 +0100

    New exception for user

---
+++ Exceptions/UserNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryAPI.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    public Guid UserId { get; }
+
+    public UserNotFoundException(Guid userId)
+        : base($"User with ID {userId} does not exist.")
+    {
+        UserId = userId;
+    }
+
+    public UserNotFoundException(Guid userId, string message)
+        : base(message)
+    {
+        UserId = userId;
+    }
+}
