e51bf435e7e4cd5e53db816ac46e8814440a8402
DreKurdeK <radziak.daniel@o2.pl>
2024-11-25 12:45:55 +0100

    Added controller for register and login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryAPI.Models;
+using LibraryAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(IUserService userService) : ControllerBase
+{
+    private readonly IUserService _userService = userService;
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequest registerRequest)
+    {
+        var user = await _userService.RegisterAsync(registerRequest);
+        return Ok(new { user.Id, user.Username,user.Email });
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest loginRequest)
+    {
+        var token = await _userService.LoginAsync(loginRequest);
+        return Ok(new { Token = token });
+    }
+}
