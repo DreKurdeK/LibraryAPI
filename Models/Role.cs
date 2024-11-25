dadf68e414f908b99ac1227174bc4ca1a8941760
DreKurdeK <radziak.daniel@o2.pl>
2024-11-25 16:07:34 +0100

    Removed unnecesary line

--- Models/Role.cs
+++ Models/Role.cs
@@ -5,5 +5,5 @@ public class Role
     public int Id { get; set; }
     public required string Name { get; set; }
     
-    public ICollection<UserRole> UserRoles { get; set; } = new List<User>();
+    public ICollection<UserRole> UserRoles { get; set; }
 }
