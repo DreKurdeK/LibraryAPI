e451c2d6caf166875ec315e4f47a0a9b446daa20
DreKurdeK <radziak.daniel@o2.pl>
2024-11-25 12:43:53 +0100

    Added User, UserRoles and Roles to database

--- Data/LibraryDbContext.cs
+++ Data/LibraryDbContext.cs
@@ -10,4 +10,26 @@ public class LibraryDbContext : DbContext
     public DbSet<Book> Books { get; set; }
     public DbSet<Author> Authors { get; set; }
     public DbSet<Publisher> Publishers { get; set; }
+    
+    public DbSet<User> Users { get; set; }
+    
+    public DbSet<Role> Roles { get; set; }
+    
+    public DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserRole>()
+            .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(u => u.User)
+            .WithMany(ur => ur.UserRoles)
+            .HasForeignKey(ur => ur.UserId);
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(r => r.Role)
+            .WithMany(ur => ur.UserRoles)
+            .HasForeignKey(ur => ur.RoleId);
+    }
 }
